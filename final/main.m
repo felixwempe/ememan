2ae724f4ba8c805c8ac7636d0fee1875cf8f3d87
Felix Wempe <felix-wempe@live.com>
2015-11-11 21:02:51 +0100

    test data added

--- final/main.m
+++ final/main.m
@@ -17,19 +17,20 @@ sequence = '00';
 
 accuracy = 0.0000001;
 algorithm = 'helmke';
-numb_frames =21;
+numb_frames =200;
 save_dir = [direct, 'results/'];
 
 %% Read the data. 
 % M1 and M2 are structs containing in each cell the points from image1 and
 % image2 for which the essentiell matrix shall be estimated.
-[M1, M2] = data_read(data_dir, Calib_mat, numb_frames);
-
+%[M1, M2] = data_read(data_dir, Calib_mat, numb_frames);
+load('matched_1.mat');
+load('matched_2.mat');
 %% Read the groundtruth data.
 % Into a struct P containing rotation and translation groundtruth
 % informations of each image pair.
-P = gt_read(gt_dir, sequence, numb_frames);
-
+%P = gt_read(gt_dir, sequence, numb_frames);
+load('projections.mat');
 %% Run the algorithm on all frames.
 
 [Estimate, Rotation, Translation] = evaluate_alg( M1, M2, P, U_init, V_init,...
